0229e85c4d67b4663d7042fc38df9cdaad18441b
Ruchir Singh <ruchirsingh@Ruchirs-MacBook-Pro.local>
2021-01-05 12:34:46 +0800

    added base project structure

---
+++ server.js
@@ -0,0 +1,29 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const expressEjsLayout = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const indexRouter = require('./routes/index')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+
+app.use(expressEjsLayout)
+app.use(express.static('public'))
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('connected to mongoose'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 5000)
